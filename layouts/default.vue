946720111b4d474237b7a89d66aeded304cd2fd1
erickhanon <erick.hanon@gmail.com>
2022-09-10 23:44:19 -0300

    First Commit

---
+++ layouts/default.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+      <v-list>
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <div class="ml-2">
+        <v-btn icon @click.stop="miniVariant = !miniVariant">
+          <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        </v-btn>
+      </div>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn icon @click="themeToggle">
+        <v-icon>mdi-{{ themeIcon }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>Erick Hanon &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      themeIcon: 'lightbulb-on',
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Lista de Atividades'
+    }
+  },
+  methods: {
+    themeToggle () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      this.themeIcon = this.$vuetify.theme.dark ? 'lightbulb' : 'lightbulb-on'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <v-card class="pa-5 ma-5">
+      <h1>To do list</h1>
+      <p>Crie uma nova tarefa</p>
+      <div class="create-new">
+        <v-text-field type="text" v-model="newTask" placeholder="Add a new task" @keypress.enter="addTask" />
+        <v-btn @click="addTask">Adicionar</v-btn>
+      </div>
+    </v-card>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
